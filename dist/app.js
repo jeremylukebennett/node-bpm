57faa2314df8b280dde6fe556b60ddd3aa78326e
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-06-07 15:45:00 -0500

    simple bpm counter working in browser

--- dist/app.js
+++ dist/app.js
@@ -3,4 +3,64 @@
 
 
 console.log("connected?");
+
+var BPM = require('bpm'),
+    b = new BPM();
+
+let button = document.getElementById("tap");
+
+button.addEventListener("click", function(){
+    console.log("click");
+    
+    setTimeout(function() {
+      console.log(b.tap());
+    }, 1000);
+});
+
+},{"bpm":2}],2:[function(require,module,exports){
+/**
+ * Calculate BPM
+ */
+
+if (typeof exports !== 'undefined') {
+  module.exports = BPM;
+  module.exports.BPM = BPM;
+}
+
+function BPM() {
+  this.count = 0;
+  this.ts = 0;
+  this.old_ts = 0;
+}
+
+BPM.prototype.tap = function() {
+  this.ts = Date.now();
+  if (!this.first_ts) this.first_ts = this.ts;
+
+  var ret = {};
+
+  // ignore the first tap
+  if (this.old_ts) {
+    var ms = this.ts - this.old_ts;
+
+    var avg = 60000 * this.count / (this.ts - this.first_ts);
+
+    ret.avg = avg;
+    ret.ms = ms;
+  }
+
+  ret.count = ++this.count;
+
+  // Store the old timestamp
+  this.old_ts = this.ts;
+  return ret;
+};
+
+BPM.prototype.reset = function() {
+  this.count = 0;
+  this.ts = 0;
+  this.old_ts = 0;
+  this.first_ts = 0;
+};
+
 },{}]},{},[1]);
--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@
 
 
 console.log("connected?");
+
+var BPM = require('bpm'),
+    b = new BPM();
+
+let button = document.getElementById("tap");
+
+button.addEventListener("click", function(){
+    console.log("click");
+    
+    setTimeout(function() {
+      console.log(b.tap());
+    }, 1000);
+});
